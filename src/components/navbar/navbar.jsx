9a0cf510aab30dcc8df311c426ccd30af51c8cf6
IbrahimBashir123 <ibrahimbashir11111@gmail.com>
2023-04-20 11:22:04 +0300

    initialize navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+import logo from "./logo.svg";
+
+function NavBar() {
+  return (
+    <div>
+      <img src={logo}></img>
+    </div>
+  );
+}
+
+export default NavBar;
