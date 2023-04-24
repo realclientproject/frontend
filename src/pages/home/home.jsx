7e50da6d231d9506682e26b0f6aa0b85e94649cb
IbrahimBashir123 <ibrahimbashir11111@gmail.com>
2023-04-24 15:18:04 +0300

    Navbar & Footer Done!

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Hero from "../../components/hero/hero";
 import NavBar from "../../components/navbar/navbar";
+import Card from "../../components/Card/card";
+import Footer from "../../components/Footer/footer";
 function Home() {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Card/>
+      <Footer />
     </div>
   );
 }
