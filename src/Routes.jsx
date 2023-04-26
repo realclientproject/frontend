756ed702a7c4b37535558b049d244090fdc5d661
IbrahimBashir123 <119161397+IbrahimBashir123@users.noreply.github.com>
2023-04-26 12:00:35 +0300

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -11,11 +11,12 @@ import Profile from "./pages/profile/profile.jsx";
 import Home from "./pages/home/home.jsx";
 import SignUp from "./pages/signup/signup.jsx";
 import Quizze from "./pages/quizzes/quizzes.jsx";
+import AdminLogin from "./pages/login/adminlogin.jsx";
 
 const AllRoutes = () => {
 	return (
-		<Router>
-        <Routes>
+	<Router>
+         <Routes>
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/help" element={<Help />}></Route>
           <Route exact path="/dashboard" element={<Admin />}></Route>
@@ -24,6 +25,7 @@ const AllRoutes = () => {
           <Route exact path="/policies" element={<Policy />}></Route>
           <Route exact path="/lessons" element={<Lesson />}></Route>
           <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/adminpanel" element={<AdminLogin />}></Route>
           <Route exact path="/signup" element={<SignUp />}></Route>
           <Route exact path="/quizzes" element={<Quizze />}></Route>
         </Routes>
