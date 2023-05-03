9ac84ba69f9ebae75c175148b8ab69357ecab427
fatima-katrib <fatimakatrib19@gmail.com>
2023-05-03 07:36:25 +0300

    fix design of sidebar

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -9,6 +9,8 @@ import {
   IconButton,
   useMediaQuery,
   Typography,
+  Divider,
+  Toolbar,
 } from "@mui/material";
 import {
   Dashboard as DashboardIcon,
@@ -20,6 +22,9 @@ import {
   Logout as LogoutIcon,
   AddBox as AddBoxIcon,
 } from "@mui/icons-material";
+import QuizIcon from "@mui/icons-material/Quiz";
+import BookIcon from "@mui/icons-material/Book";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
 import { Box } from "@mui/system";
 import Logo from "../../images/logoo.png";
 
@@ -38,7 +43,7 @@ function Sidebar({ isMenuOpen, setIsMenuOpen }) {
     },
     {
       text: "Admins",
-      icon: <PersonIcon />,
+      icon: <SupervisorAccountIcon />,
       path: "/admins",
     },
     {
@@ -48,12 +53,12 @@ function Sidebar({ isMenuOpen, setIsMenuOpen }) {
     },
     {
       text: "Lessons",
-      icon: <TransactionsIcon />,
+      icon: <BookIcon />,
       path: "/admin/lessons",
     },
     {
       text: "Quizzes",
-      icon: <TransactionsIcon />,
+      icon: <QuizIcon />,
       path: "/admin/quizzes",
     },
     {
@@ -82,120 +87,106 @@ function Sidebar({ isMenuOpen, setIsMenuOpen }) {
   ];
 
   //need check role of admin
-  
+
   const drawer = (
-    <List
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        width: "315px",
-        height: "100vh",
-      }}
-    >
-      <Box sx={{ marginX: "14px" }}>
+    <div>
+      <Toolbar>
         <img
           src={Logo}
           alt="logo"
-          style={{ height: "40px", marginBottom: "36px" }}
+          style={{ height: "40px", alignSelf: "center", margin: "auto" }}
         />
-        <Typography mt={2} color="GrayText" paddingX="16px" paddingY="8px">
-          User Panel
-        </Typography>
-        {SuperAdminDrawerItems.map((item, index) => (
-          <ListItem
-            button
-            component={Link}
-            to={item.path}
-            key={item.path}
-            onClick={handleDrawerToggle}
-            style={{
-              backgroundColor:
-                window.location.pathname === item.path
-                  ? "rgba(2, 111, 194, 0.1)"
-                  : "",
-              color: window.location.pathname === item.path ? "#026FC2" : "",
-            }}
-          >
-            <ListItemIcon
+      </Toolbar>
+      <Divider />
+      <List
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          width: "315px",
+        }}
+      >
+        <Box sx={{ marginX: "14px" }}>
+          <Typography mt={2} color="GrayText" paddingX="16px" paddingY="8px">
+            User Panel
+          </Typography>
+          {SuperAdminDrawerItems.map((item, index) => (
+            <ListItem
+              button
+              component={Link}
+              to={item.path}
+              key={item.path}
+              onClick={handleDrawerToggle}
               style={{
+                backgroundColor:
+                  window.location.pathname === item.path
+                    ? "rgba(2, 111, 194, 0.1)"
+                    : "",
                 color: window.location.pathname === item.path ? "#026FC2" : "",
               }}
             >
-              {item.icon}
-            </ListItemIcon>
-            <ListItemText primary={item.text} />
-          </ListItem>
-        ))}
-      </Box>
-      <Box sx={{ marginX: "14px" }}>
-        <ListItem
-          button
-          selected={window.location.pathname === "/help"}
-          onClick={handleDrawerToggle}
-        >
-          <ListItemIcon>
-            <HelpIcon />
-          </ListItemIcon>
-          <ListItemText>Help</ListItemText>
-        </ListItem>
-        <ListItem
-          button
-          selected={window.location.pathname === "/logout"}
-          onClick={handleDrawerToggle}
-        >
-          <ListItemIcon>
-            <LogoutIcon />
-          </ListItemIcon>
-          <ListItemText>Logout</ListItemText>
-        </ListItem>
-      </Box>
-    </List>
+              <ListItemIcon
+                style={{
+                  color:
+                    window.location.pathname === item.path ? "#026FC2" : "",
+                }}
+              >
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </Box>
+        
+      </List>
+    </div>
   );
 
   return (
     <>
-      {isMobile ? (
-        <div>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Drawer
-            variant="temporary"
-            anchor="left"
-            open={isMenuOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-              keepMounted: true,
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </div>
-      ) : (
-        <Box
-          variant="permanent"
-          anchor="left"
-          maxWidth="315px"
-          sx={{
-            background: "white",
-            border: "1px solid rgba(109, 125, 147, 0.15)",
-            boxShadow: "4px 4px 20px -10px rgba(0, 0, 0, 0.1)",
-            // position: "fixed",
-            // zIndex: "1",
-            // top: "0",
-            // left: "0",
-            // overflowX: "hidden",
-          }}
+      {/* <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
         >
-          {drawer}
-        </Box>
-      )}
+          <MenuIcon />
+        </IconButton> */}
+      <Drawer
+        variant="temporary"
+        anchor="left"
+        open={isMenuOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{ display: ["flex", "none"] }}
+      >
+        {drawer}
+      </Drawer>
+
+      {/* //desktop */}
+
+      <Box
+        variant="permanent"
+        anchor="left"
+        maxWidth="315px"
+        sx={{
+          background: "white",
+          border: "1px solid rgba(109, 125, 147, 0.15)",
+          boxShadow: "4px 4px 20px -10px rgba(0, 0, 0, 0.1)",
+          // position: "fixed",
+          // zIndex: "1",
+          // top: "0",
+          // left: "0",
+          // overflowX: "hidden",
+          display: ["none", "block"],
+          height: "100vh",
+
+        }}
+      >
+        {drawer}
+      </Box>
     </>
   );
 }
